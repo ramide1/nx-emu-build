149c075514254a8226ff7fe822ac6eaaa7e7687f
Ramiro <ramiro.depaoli@gmail.com>
2025-03-12 16:23:38 -0300

    try fix torzu

--- torzu_linux.sh
+++ torzu_linux.sh
@@ -20,8 +20,7 @@ echo "⚒️Building bin...⚒️"
 ninja && echo "✔️⚒️Bin builded correctly⚒️✔️" || echo "❌⚒️Error building bin⚒️❌"
 echo "⚒️Building appimage...⚒️"
 cd ..
-mkdir -p AppImageBuilder/build
-cp /usr/lib/libSDL3.so* ./AppImageBuilder/build/
+mkdir -p AppImageBuilder/build && cp /usr/lib/libSDL3.so* AppImageBuilder/build
 ./AppImage-build.sh && echo "✔️⚒️Appimage builded correctly⚒️✔️" || echo "❌⚒️Error building appimage⚒️❌"
 chmod +x *.AppImage && echo "✔️Permissions updated correctly✔️" || echo "❌Error updating permissions❌"
 mv *.AppImage "$WORKING_DIR/outputs/torzu.AppImage"
